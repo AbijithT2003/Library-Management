7663376f398ec0994f3bde9d97cc99ca9aa3a10d
AbijithT2003 <jitthu186@gmail.com>
2025-06-24 13:54:23 +0530

    lombok constructers set end remover boilerplate code

--- books/src/main/java/librarymanagement/books/model/Book.java
+++ books/src/main/java/librarymanagement/books/model/Book.java
@@ -30,51 +30,7 @@ public class Book {
     @Column(name = "published_year")
     private int publishedYear;
 
-    public Book() {}
-
-    public Book(Long id, String title, String author, String genre, int publishedYear) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.publishedYear = publishedYear;
     }
 
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public int getPublishedYear() {
-        return publishedYear;
-    }
-    public void setPublishedYear(int publishedYear) {
-        this.publishedYear = publishedYear;
-    }
-}
-
+    
+    
